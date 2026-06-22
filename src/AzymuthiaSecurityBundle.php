d0c22c18201bd66186adf044c17dc06caa6a2b72
Bartosz Piotr Pazola <mmmalfc@gmail.com>
2026-06-22 12:18:11 +0200

    refactor: replace attribute with autoconfiguration for AppUserRepositoryInterface and register autoconfiguration in AzymuthiaSecurityBundle

--- src/AzymuthiaSecurityBundle.php
+++ src/AzymuthiaSecurityBundle.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Azymuthia\SecurityBundle;
 
+use Azymuthia\SecurityBundle\Contract\AppUserRepositoryInterface;
 use Azymuthia\SecurityBundle\DependencyInjection\AzymuthiaSecurityExtension;
 use Azymuthia\SecurityBundle\DependencyInjection\Compiler\AppUserAutowirePass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -15,6 +16,12 @@ final class AzymuthiaSecurityBundle extends Bundle
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);
+
+        $container
+            ->registerForAutoconfiguration(AppUserRepositoryInterface::class)
+            ->addTag('azymuthia.security.app_user_repository')
+        ;
+
         $container->addCompilerPass(new AppUserAutowirePass());
     }
 
--- src/Contract/AppUserRepositoryInterface.php
+++ src/Contract/AppUserRepositoryInterface.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Azymuthia\SecurityBundle\Contract;
 
-use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
 use Symfony\Component\Uid\Uuid;
 
-#[AsTaggedItem('azymuthia.security.app_user_repository')]
 interface AppUserRepositoryInterface
 {
     public function getOneByUserId(Uuid $id): AppUserInterface;
